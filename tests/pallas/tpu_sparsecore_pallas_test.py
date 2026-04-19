0fd2b9010a97550005da14407f44918c8bc0aa04
Tomás Longeri <tlongeri@google.com>
2026-04-18 22:16:21 -0700

    [Pallas] Support take_along_axis in SC
    Caveat: Like TC, it might not work when there is a dimension of size 1 in the source array, due to JAX lowering it with a different pattern
    PiperOrigin-RevId: 902006679

--- tests/pallas/tpu_sparsecore_pallas_test.py
+++ tests/pallas/tpu_sparsecore_pallas_test.py
@@ -1964,6 +1964,26 @@ class VectorSubcoreTest(PallasSCTest):
 
     np.testing.assert_array_equal(kernel(x, indices), x[indices])
 
+  @parameterized.product(dim=[0, 1, 2], dtype=[np.int32, np.float32])
+  def test_take_along_axis(self, dim, dtype):
+    shape = (2, 8, 64)
+    key = jax.random.key(1234)
+    x = np.arange(math.prod(shape), dtype=dtype).reshape(shape)
+    indices = jax.random.randint(key, shape, 0, shape[dim], dtype=np.int32)
+
+    @self.vector_subcore_kernel(
+        out_shape=x,
+        compiler_params=pltpu.CompilerParams(needs_layout_passes=True),
+    )
+    def kernel(x_ref, indices_ref, out_ref):
+      out_ref[...] = jnp.take_along_axis(
+          x_ref[...], indices_ref[...], axis=dim, mode="promise_in_bounds"
+      )
+
+    np.testing.assert_array_equal(
+        kernel(x, indices), jnp.take_along_axis(x, indices, axis=dim)
+    )
+
   @parameterized.product(
       keys_dtype=[np.uint32, np.int32, np.float32],
       values_dtype=[np.int32, np.float32],
